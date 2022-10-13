76ec5dbeb86e356ee2d8b9406e1ffe4dcbb5e4d0
Lowkeyoj <rockyojaji@gmail.com>
2022-10-12 21:26:36 -0700

    Made a function for the computer that runs automaticaly

--- java.js
+++ java.js
@@ -1,12 +1,14 @@
 
+let random = Math.random();  
+let X = .33;
+let Y = .66;
+let Z = .99;
 
-
-
-function getComputerChoice(X, Y, Z){
- if (X, Y, Z) { return ('Rock');
-} else if (X, Y) {
+function getComputerChoice(){
+ if (random < Z && random > Y) { return ('Rock');
+} else if  (random < Y && random > X) {
  return ('Paper');
-} else (X) 
+} else (random < X) 
  return ('Scissors');
 }
 
@@ -22,13 +24,12 @@ function getPlayerChoice(A, B, C){
 
  
 let playerSelection = getPlayerChoice;
-    computerSelection = getComputerChoice;
-    
+    computerSelection = getComputerChoice();
+ 
+
 function gamePlay(playerSelection,computerSelection){
   if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
-    alert('were onto something');
+    alert('');
   } 
 }
 
-
-console.log(gamePlay('rock','paper'));
