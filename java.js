04ab7efd3a933fcecdc2d3fd8b324be57b2592a3
Lowkeyoj <rockyojaji@gmail.com>
2022-10-12 17:59:01 -0700

    GetComputerChoice code made

---
+++ java.js
@@ -0,0 +1,10 @@
+function getComputerChoice(X, Y, Z){
+ if (X, Y, Z) { return ('Rock');
+} else if (X, Y) {
+ return ('Paper');
+} else (X) 
+ return ('Scissors');
+}
+
+
+console.log(getComputerChoice(1, 2,));
