157183f88ebab01d025d3fd647489687b05ea1c9
Lowkeyoj <rockyojaji@gmail.com>
2022-11-11 10:10:43 -0800

    fixed logic error

--- java.js
+++ java.js
@@ -5,7 +5,7 @@ let Z = 10;
 
 
 function getComputerChoice() {
-    if(Math.floor(Math.random() * 10)  < Z && Math.floor(Math.random() * 10) > Y) {
+    if (Math.floor(Math.random() * 10) < Z && Math.floor(Math.random() * 10) > Y) {
         return ('Rock');
     } else if (Math.floor(Math.random() * 10) < Y && Math.floor(Math.random() * 10) > X) {
         return ('Paper');
@@ -31,35 +31,33 @@ function gamePlay(playerSelection) {
         return ('Player wins');
     } else if (playerSelection.toLowerCase() == 'scissors' && getComputerChoice().toLowerCase() == 'rock') {
         return ('Computer wins');
-    } 
+    } else return 'There has been a tie';
 }
 
 //Test function calling the gamePlay(PlayerSelection) function keep score for first to five//
 
 function keepScore(playerSelection) {
 
-    if (gamePlay(playerSelection) == 'Computer wins') 
-           list.innerHTML = `${Computer++} Computer score`;
-       
-    else if (gamePlay(playerSelection) == 'Player wins') 
-            list.innerHTML = `${Player++} Player score`;
-                            
-            else if(Player === 3){
-                alert('Player wins');
-                list.innerHTML = `${Player++} Player score`;
-                list2.innerHTML = 'Game over refresh to play again player won this round'
-            }
-            else if (Computer === 3){
-                alert('Computer wins');
-                list.innerHTML = `${Computer++} Computer score`;
-                list2.innerHTML = 'Game over refresh to play again computer won' 
-                        
-            }  
-                else list.innerHTML = 'Its a tie play again';
 
-                                        
+     if (Player === 3) {
+        alert('Player wins');
+        list.innerHTML = `${Player} Player score`;
+        list2.innerHTML = 'Game over refresh to play again player won this round'
+    }
+    else if (Computer === 3) {
+        alert('Computer wins');
+        list.innerHTML = `${Computer} Computer score`;
+        list2.innerHTML = 'Game over refresh to play again computer won'
+    }
+    
+    else if(gamePlay(playerSelection) == 'Computer wins')
+        list.innerHTML = `${Computer++} Computer score`;
 
- 
+    else if (gamePlay(playerSelection) == 'Player wins')
+        list.innerHTML = `${Player++} Player score`;
+
+    else list.innerHTML = 'Its a tie play again';
+
 
 }
 
@@ -80,7 +78,7 @@ const bt1 = document.getElementById('bt1');
 const bt2 = document.getElementById('bt2');
 const bt3 = document.getElementById('bt3');
 
-let num1 = bt1.onclick = () => keepScore('Rock'); ;
-let num2 = bt2.onclick = () => keepScore('Paper'); ;
-let num3 = bt3.onclick = () => keepScore('Scissors'); ;
+let num1 = bt1.onclick = () => keepScore('Rock');;
+let num2 = bt2.onclick = () => keepScore('Paper');;
+let num3 = bt3.onclick = () => keepScore('Scissors');;
 
